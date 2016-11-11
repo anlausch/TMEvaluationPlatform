e5751897e8908e008a66544a49c513ca71b549b7
anlausch <anne-lauscher@web.de>
2016-11-11 16:12:57 +0100

    [REFACTORING] add config.js and put db info there

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.db = {};
+
+config.db.host = "localhost";
+config.db.user = "root";
+config.db.password = "root";
+config.db.schema = "europarl";
+
+module.exports = config;
